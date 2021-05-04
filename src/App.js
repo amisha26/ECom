706f065cdb441e825a48145d28315671f7482549
Amisha Tiwari <amisha26>
2021-05-04 14:01:10 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-      <h1>Welcome to ProShop</h1>
-      </Fragment>
-
+      <div>
+        <Header />
+        <main>
+        <h1>hello</h1>
+        </main>
+        <Footer />
+      </div>
     );
   }
 }
