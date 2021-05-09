98560b0754d7d3e54b8357525ce01add03d41074
Amisha Tiwari <amisha26>
2021-05-09 17:24:54 +0530

    server,nodemon

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from "react"; //useEffect makes a request to the backend
 import { Row, Col } from "react-bootstrap";
-import products from '../products';
-import Product from '../components/Product';
+//import products from '../products';
+import Product from "../components/Product";
+import axios from "axios";
 
-const HomeScreen = () => {
-    return (
-        <div>
-        <h1>Latest Products</h1>
-            <Row>
-            {products.map(product => 
-                (<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-                </Col>))}
-            </Row>
-        </div>
-    )
+  const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  //useEffect takes an arrow func. whatever we put in useEffect is going to run as soon as the component loads.
+  // we can use axios.get() but instead we create another func inside useEffect
+  useEffect(() => {
+    const fetchProducts = async () => {
+      //async() = an asynchronous function
+      const { data } = await axios.get("/api/products"); // const res= ---
+      setProducts(data); // setProducts(res.data) 
+    }
+    fetchProducts()
+  },[]); //for second argument we pass in dependency
+
+  return (
+    <div>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 };
 
 export default HomeScreen;
